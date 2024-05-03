7a5d27c3f3c9b5887f7193c2db344eb25dbd030f
JeremiahJen <157443280+JeremiahJen@users.noreply.github.com>
2024-05-03 18:15:34 -0500

    Update Final_Project_Code.m

--- Project 4/Final_Project_Code.m
+++ Project 4/Final_Project_Code.m
@@ -10,9 +10,9 @@ path.radius = track_radius;
 [xp,yp] = trackinfo(track_length,track_radius,0,delta_s,delta_theta);
 
 set_param('Final_Project', 'StopTime', '3600')
+simout = sim("Final_Project.slx");
 X = simout.X.data;
 Y = simout.Y.data;
-simout = sim("Final_Project.slx");
 stats = raceStat(X,Y,simout.tout,path,simout);
 disp(stats)
 
